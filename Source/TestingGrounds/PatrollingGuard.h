f3d50b553aa43952798f425473fb378a6dcff7f0
Kevin <kevin.amores.m@gmail.com>
2018-12-20 22:30:35 +0100

    Lesson 19 Making the AI on C++

--- Source/TestingGrounds/PatrollingGuard.h
+++ Source/TestingGrounds/PatrollingGuard.h
@@ -13,8 +13,9 @@ UCLASS()
 class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
-	
-	
-	
+
+public:
+		UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+		TArray<UObject*> PatrolPointsCPP;
 	
 };
